1ce87afed56b59afd9fc67b138afe8d7d03b6d8e
Shabbyshambles <57321784+Shabbyshambles@users.noreply.github.com>
2020-01-21 18:51:46 -0500

    Add files via upload

--- tornadoserver.py
+++ tornadoserver.py
@@ -7,7 +7,7 @@ import tornado.web
 class Main_Handler(tornado.web.RequestHandler):
     def get(self):
         
-        self.render("index.html", title="MyTitle")
+        self.render("index1.html", title="MyTitle")
 #recieves the form data from /1 and posts a response on /2
 
 
@@ -24,7 +24,13 @@ class Vote_Handler(tornado.web.RequestHandler):
     def post(self):
         self.render("Vote_Submit.html")
     get = post
-
+class Update_Handler(tornado.web.RequestHandler):
+    def post(self):
+        update1 = self.get_argument(name = "mytextarea")
+        update2 = self.get_argument(name = "mychoice1")
+        update3 = self.get_argument(name = "mychoice2")
+        self.render("update.html",U1=update1,U2=update2,U3=update3)
+    get = post
         
 
 
@@ -33,9 +39,10 @@ class Vote_Handler(tornado.web.RequestHandler):
 if __name__ == "__main__":
     app = tornado.web.Application([
         
-        (r"/1", Main_Handler),
+        (r"/", Main_Handler),
         (r"/2", Submit_Handler),
-        (r"/3", Vote_Handler)
+        (r"/3", Vote_Handler),
+        (r'/4', Update_Handler)
        
        
     ])
